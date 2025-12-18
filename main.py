3aea878dc498a335a28f2dcb9cc128c3daf02e6a
ygs <ygs3004@naver.com>
2025-12-18 18:35:49 +0900

    당근, 변수, 물주기, 함수 해금

--- main.py
+++ main.py
@@ -6,15 +6,35 @@ clear()
 # NxN 이동 심기
 while True:
 
+    # for i in range(get_world_size()):
+    #     # 나무
+    #     harvest()
+    #
+    #     if get_water() < 0.5 and num_items(Items.Water) > 10:
+    #         use_item(Items.Water)
+    #
+    #     plant(Entities.Bush)
+    #
+    #     move(North)
+    #
+    # move(East)
+
+
     for i in range(get_world_size()):
-        # 나무
-        if not plant(Entities.Bush):
-            harvest()
-            plant(Entities.Bush)
+        # 당근
+        harvest()
+
+        if get_water() < 0.2 and num_items(Items.Water) > 10:
+            use_item(Items.Water)
+
+        if not plant(Entities.Carrot):
+            till()
+            plant(Entities.Carrot)
 
         move(North)
 
     move(East)
+
 
 # 풀
 # for i in range(get_world_size()):
@@ -22,3 +42,7 @@ while True:
 #     move(North)
 #
 # move(East)
+
+# 당근
+# till()
+# plant(Entities.Carrot)
--- memo.py
+++ memo.py
@@ -9,7 +9,8 @@ change_hat(Hats.Gray_Hat)
 change_hat(Hats.Purple_Hat)
 change_hat(Hats.Green_Hat)
 change_hat(Hats.Brown_Hat)
-
+change_hat(Hats.Tree_Hat)
+change_hat(Hats.Carrot_Hat)
 
 # 수확 가능 체크
 can_harvest()
@@ -23,7 +24,7 @@ move(East)
 move(South)
 move(West)
 
-# 나무 심기
+# 심기
 # 지정된 entity 비용을 지불하고 드론 아래에 심습니다.
 # 비용이 부족하거나, 땅 종류가 틀리거나, 이미 식물이 심어져 있으면 실패.
 # 성공 True, False를 반환
@@ -64,3 +65,30 @@ get_ground_type()
 # 해금 체크
 num_unlocked(Unlocks.Speed)
 num_unlocked(Unlocks.Senses)
+
+# 당근 심기(땅갈기 => 당근), 나무와 건초 소모
+till() # Grounds.Grassland <=> Grounds.Soil
+plant(Entities.Carrot)
+
+# 화면 출력
+print()
+# 기록
+quick_print()
+
+
+# 물 주기
+# 물의 범위 0 ~ 1
+# 0에서 기본 속도, 1에서 5배 속도
+# 10초에 물 탱크 하나가 인벤 토리 추가, 업그레이드당 2배
+# 물 1탱크는 0.25의 물
+# 물의 값
+get_water() 
+# 물 주기
+use_item(Items.Water)
+
+# 변수
+example_variable = "값"
+
+# 함수
+def example_function(arg1, arg2 = "default value"):
+    pass
